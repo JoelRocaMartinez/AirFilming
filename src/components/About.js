1979ca21c5879f4c92c81e02f6926fc26228fb10
Joel <joel.roca.martinez@hotmail.com>
2020-09-18 12:10:44 +0200

    services styles added

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ import "./styles/About.css";
 function About() {
     return (
         <div>
-            <h1>QUIÉNES SOMOS</h1>
+            <h1>EL EQUIPO</h1>
            
             <p>Airfilming es una operadora de RPA en la zona de Catalunya. Compuesta actualmente por dos pilotos e ingenieros los cuales hemos querido dar un nuevo enfoque a nuestro futuro profesional.
                Contamos con la aprobación de la Agencia Estatal de Seguridad Aérea (AESA) y trabajamos de manera ininterrumpida en las mejoras y la profesionalidad día a día. 
--- src/components/Services.js
+++ src/components/Services.js
@@ -7,22 +7,22 @@ function Services() {
     return (
         <div id="services">
         <div className="title">
-          <h1>SERVICIOS</h1>
+          <h2>SERVICIOS</h2>
         </div>
         
             <div className="sections">
-                <div className="section">
+                <div className="section1">
                     <h3>SEGUIMIENTO DE OBRAS</h3>
                     <p>La libertad de movimiento del dron, permite un seguimiento aéreo de cualquier infraestructura, llegando a rincones de difícil acceso, con una facilidad y un riesgo menor.
                        Genera un registro digital de la evolución de la obra, con videos y fotografías 4K para un detalle óptimo.</p>  
                 </div>
 
-                <div className="section">
+                <div className="section2">
                     <h3>FOTOGRAMETRIA</h3>
                     <p>Los Drones permiten dar un nuevo enfoque a la generación de información de un terreno, de una forma más eficiente, rápida y precisa. Desde planos de curvas de nivel hasta generar un 3D detallado de la zona fotografiada.</p>  
                 </div>
 
-                <div className="section">
+                <div className="section3">
                     <h3>MARKETING EMPRESARIAL</h3>
                     <p>¡Dale un nuevo punto de vista a tu proyecto con un vuelo dinámico y creativo, sin límites, tanto en interior como en exterior! 
                        Hay muchas posibilidades: casas rurales, oficinas, gimnasios, bares, videoclips, deportes...</p>  
