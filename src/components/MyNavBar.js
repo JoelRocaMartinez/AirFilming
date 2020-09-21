2a658a39d30396db0f4bd326e38d9f9d0ccf18f3
Joel <joel.roca.martinez@hotmail.com>
2020-09-21 17:02:54 +0200

    website status ok before review

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -7,11 +7,11 @@ function MyNavBar() {
     return (
         <div id="myNavBar">
             <div className="sections">
-                <a className="section" href="#section2">AirFilming</a>
-                <a className="section" href="#section2">Servicios</a>
-                <a className="section" href="#section2">Nuestra flota</a>
-                <a className="section" href="#section2">Quiénes Somos</a>
-                <a className="section" href="#section2">Contacto</a> 
+                <a className="section" href="#airfilming">AirFilming</a>
+                <a className="section" href="#services">Servicios</a>
+                <a className="section" href="#drones">Nuestra flota</a>
+                <a className="section" href="#about">Quiénes Somos</a>
+                <a className="section" href="#contact">Contacto</a> 
             </div>
         </div>
     )   
