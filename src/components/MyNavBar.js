09801717c697eb93d7acf845c42b0097d8975860
Joel <joel.roca.martinez@hotmail.com>
2020-09-22 11:04:52 +0200

    NavBar modified

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -7,11 +7,18 @@ function MyNavBar() {
     return (
         <div id="myNavBar">
             <div className="sections">
-                <a className="section" href="#airfilming">AirFilming</a>
-                <a className="section" href="#services">Servicios</a>
-                <a className="section" href="#drones">Nuestra flota</a>
-                <a className="section" href="#about">Quiénes Somos</a>
-                <a className="section" href="#contact">Contacto</a> 
+                <div className="primary">
+                  <a className="airfilming" href="#airfilming">AirFilming</a>
+                </div>
+
+                <div className="rest">
+                    <a className="section" href="#services">Servicios</a>
+                    <a className="section" href="#drones">Nuestra flota</a>
+                    <a className="section" href="#about">Quiénes Somos</a>
+                    <a className="section" href="#contact">Contacto</a> 
+                </div>
+                
+               
             </div>
         </div>
     )   
