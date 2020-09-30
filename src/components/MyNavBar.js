5e8f30c9e3392a2eaff877be2fa72c8c86be0b3d
Joel <joel.roca.martinez@hotmail.com>
2020-09-30 19:19:13 +0200

    modifications on the navbar

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {Link} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.css";
 import "./styles/MyNavBar.css";
 
 
 function MyNavBar() {
 
     return (
-        <div id="myNavBar">
-            <div className="sections">
+        <div>
+                <Navbar collapseOnSelect id="myNavBar" expand="lg" fixed="top" variant="dark">
+                <div id='fakeitem'></div>
+                    <Navbar.Brand className="airfilming" href="#airfilming">AIRFILMING</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="mr-auto desktop-navbar">
+                                <Nav.Link href="#services">Servicios</Nav.Link>
+                                <Nav.Link href="#drones">Nuestra flota</Nav.Link>
+                                <Nav.Link href="#about">Quiénes Somos</Nav.Link>
+                                <Nav.Link href="#contact">Contacto</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                </Navbar>
+            
+            {/* <div className="sections">
                         <a className="airfilming" href="#airfilming">AIRFILMING</a>
                         <a className="section" href="#services">Servicios</a>
                         <a className="section" href="#drones">Nuestra flota</a>
                         <a className="section" href="#about">Quiénes Somos</a>
                         <a className="section" href="#contact">Contacto</a> 
-                </div>
+                </div> */}
             {/* <div className="sections">
                 <div className="primary">
                   <a className="airfilming" href="#airfilming">AIRFILMING</a>
