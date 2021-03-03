5a74b9218fb65dd7aba6edc51193f15b7eef85ca
Joel <joel.roca.martinez@hotmail.com>
2021-03-03 12:11:11 +0100

    fixed design issues

---
+++ buildv3/precache-manifest.7c18b4483fc3dbedcb880efc72d69e91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe740e74fe8dfc51edbc7437524a3d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab005e6f20dbbf3bfc66",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c69a5669c15ee4b086d1",
+    "url": "/static/css/main.c91f87b1.chunk.css"
+  },
+  {
+    "revision": "ab005e6f20dbbf3bfc66",
+    "url": "/static/js/2.0d61dc0d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0d61dc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69a5669c15ee4b086d1",
+    "url": "/static/js/main.b7e92959.chunk.js"
+  },
+  {
+    "revision": "147391fc6db4207328a5",
+    "url": "/static/js/runtime-main.02c934dd.js"
+  }
+]);
