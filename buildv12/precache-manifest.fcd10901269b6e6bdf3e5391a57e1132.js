e3cef5a3bec67e5e36419d4047a49a7bffd1cf75
Joel <joel.roca.martinez@hotmail.com>
2021-03-18 11:18:35 +0100

    queries for safari didnt work. back to former version

---
+++ buildv12/precache-manifest.fcd10901269b6e6bdf3e5391a57e1132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a5d9c212ccae2bee96f6ffd256b9a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49d8043edb090ae7655d",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "bd064bb87632b7358c47",
+    "url": "/static/css/main.eb9000e2.chunk.css"
+  },
+  {
+    "revision": "49d8043edb090ae7655d",
+    "url": "/static/js/2.c30b9902.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c30b9902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd064bb87632b7358c47",
+    "url": "/static/js/main.f26bb937.chunk.js"
+  },
+  {
+    "revision": "147391fc6db4207328a5",
+    "url": "/static/js/runtime-main.02c934dd.js"
+  }
+]);
