f53920eca62297892c5f32e0bf673c1bb06e6c57
Joel <joel.roca.martinez@hotmail.com>
2021-04-26 08:23:02 +0200

    version 16 release

---
+++ buildv16/precache-manifest.67b876950f9e3928a9cc3790863efd8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803590052141c9fe25df0ee8d8d6e63f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1f82490a294eaf7b6ab",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8ae85ddec3729e9a8e71",
+    "url": "/static/css/main.05f9ff23.chunk.css"
+  },
+  {
+    "revision": "d1f82490a294eaf7b6ab",
+    "url": "/static/js/2.202e0b0b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.202e0b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae85ddec3729e9a8e71",
+    "url": "/static/js/main.ada59084.chunk.js"
+  },
+  {
+    "revision": "147391fc6db4207328a5",
+    "url": "/static/js/runtime-main.02c934dd.js"
+  }
+]);
